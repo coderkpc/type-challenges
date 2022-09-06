d9028de280b1267f230c2576a89e0bea922379b4
柯芃丞 <kepengcheng314@163.com>
2022-09-06 23:20:22 +0800

    feat(11): easy-turple-to-object

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,9 @@
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly any[]> = {
+  [key in T[number]]: key
+}
+
+// 数组本质上是一个key为数字的对象
+// 所以元组的类型其实可以用T[number]取
+// 取到的是一个联合类型
+// 然后用 in 遍历
+// 由于元祖的特性 key: key即可变成对象
