c83456de5ca3aa259fa226d779cf4b885cea1005
柯芃丞 <kepengcheng314@163.com>
2022-09-06 23:05:10 +0800

    feat(7): easy-readonly

--- questions/00007-easy-readonly/template.ts
+++ questions/00007-easy-readonly/template.ts
@@ -1,1 +1,7 @@
-type MyReadonly<T> = any
+type MyReadonly<T> = {
+  readonly [key in keyof T]: T[key]
+}
+// 1. 先用keyof将interface T变成一个联合类型
+// 2. 然后用in操作符去取
+// 3. 然后拿到key去T中取
+// 4. 加上readonly关键字即可
